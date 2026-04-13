f852965cf20e0e21c8fa2e48707b52f3f9cfef43
wkastro <wcastro@aumenta.do>
2026-04-13 10:09:26 -0600

    refactor: migrate certificados page to VehicleListingGrid

--- app/(site)/certificados/page.tsx
+++ app/(site)/certificados/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import { HomeSearchBarContent } from "@/features/filters/components/home-search-bar-content";
-import { CertificadosVehicleGrid } from "@/features/certificados/components/certificados-vehicle-grid";
+import { VehicleListingGrid } from "@/features/filters/components/vehicle-listing-grid";
 import { VehicleCardSkeletonGrid } from "@/components/global/vehicle-card-skeleton";
 import { FilterLoadingProvider } from "@/features/filters/context/filter-loading-context";
 import type { SearchParams } from "@/types/filters/filters";
@@ -19,7 +19,11 @@ export default async function CertificadosPage({
       </Suspense>
 
       <Suspense fallback={<VehicleCardSkeletonGrid count={9} />}>
-        <CertificadosVehicleGrid searchParams={searchParams} />
+        <VehicleListingGrid
+          searchParams={searchParams}
+          lockedFilters={{ etiqueta: "autos-certificados" }}
+          title="Autos Certificados"
+        />
       </Suspense>
     </FilterLoadingProvider>
   );
